781c7aaae253c46136ee14371864843631a53c7e
ye546 <anton_bosse@hotmail.com>
2017-10-09 16:32:43 +0200

    code update
    added a check to see if the map was in the previous maprotation

--- Source.cpp
+++ Source.cpp
@@ -5,12 +5,15 @@
 #include <string>
 #include <iostream>
 #pragma warning(disable : 4996) //ignore the deprecated bs
+
 int main() {
 	srand(time(NULL));
 	time_t t = time(0);
 	struct tm * now = localtime(&t);
-
-
+	std::string prevMaps[12];
+	for (int i = 0; i < 12; i++) {
+		std::cin >> prevMaps[i];
+	}
 	std::string maps[] = {"up01_4worlds", "up01_a51", "up01_annihilation", "up01_assault", "up01_asturzom", "up01_asylum", "up01_auferstehung", "up01_ayatown",
 		"up01_bangbang", "up01_barrack44", "up01_battalion", "up01_bier", "up01_bodesteinv2", "up01_bromberg", "up01_brombergv2", "up01_bulton", "up01_bultonv2",
 		"up01_canismajoris", "up01_cars", "up01_cbble", "up01_cbblev2", "up01_chamberofsecrets", "up01_chateauclassic", "up01_clavarlaespada", "up01_common",
@@ -31,9 +34,16 @@ int main() {
 	newMappack << "\n\n\n";
 	newMappack << "new rotation: " << (now->tm_year + 1900) << "/" << (now->tm_mon + 1) << "/" << now->tm_mday << " " << now->tm_hour << "-" << now->tm_min << "-" << now->tm_sec <<"\n";
 	newMappack << "gametype zom ";
+	
 	for (int i = 0; i < 12; i++) {
-		newMappack << maps[(rand() % length)];
-		newMappack << " map ";
+		maps[(rand() % length)];
+		if (maps[(rand() % length)] == prevMaps[i]) {
+			continue;
+		}
+		else{
+			newMappack << maps[(rand() % length)];
+			newMappack << " map ";
+		}
 	}
 	return 0;
 }
